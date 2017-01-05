7bfe28d6e08b106b32e60209924a4a322e070954
Wahyu Titis Satria Utama <wahyutitis.id@gmail.com>
2017-01-04 20:38:14 -0800

    Create Luas.c
    Script Tugas Modul 1 tentang mencari luas persegi

---
+++ Tugas Praktikum DASPRO/Modul 1/Luas.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+int p,l;
+int luas;
+
+printf("Program Luas Persegi\n");
+printf("Masukkan Nilai P\n");
+scanf("%d",&p);
+printf("Masukkan Nilai L\n");
+scanf("%d",&l);
+luas=p*l;
+printf("Luas Persegi = %d cm\n",luas);
+}

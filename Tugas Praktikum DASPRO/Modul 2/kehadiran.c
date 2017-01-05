ab1bfe04f6a0472c6dcad65db1f4a351f1a8cad0
Wahyu Titis Satria Utama <wahyutitis.id@gmail.com>
2017-01-04 20:42:50 -0800

    Script Tugas 1

---
+++ Tugas Praktikum DASPRO/Modul 2/kehadiran.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int abs,uas;
+printf("Masukkan nilai abs = ");
+scanf("%d",&abs);
+printf("Masukkan Nilai  UAS = ");
+scanf("%d",&uas);
+
+a = abs * (70/100)
+b = uas * (50/100)
+
+if (a > 70 & b > 50)
+{
+printf("Anda Lulus\n");
+}
+if (a < 70 & b < 50)
+{
+printf("Anda Tidak Lulus\n");
+}
+printf("Presentase Kehadiran : %d\n", abs);
+printf("Nilai UAS : %d\n", uas);
+}
